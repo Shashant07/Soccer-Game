1a864dc4f16f131bf1906fd7e5258e106352d1b0
shashant07 <shashantpandit@gmail.com>
2026-04-07 01:15:53 +0530

    first commit

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useContext } from 'react';
+
+const NavigationBar = () => {
+    return (
+        <div>NavigationBar</div>
+    )
+}
+
+export default NavigationBar;
---
+++ src/components/quiz/LifelineBadge.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LifelineBadge = () => {
+    return (
+        <div>LifelineBadge</div>
+    )
+}
+
+export default LifelineBadge;
